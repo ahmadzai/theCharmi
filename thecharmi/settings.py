59bdbc9af6a906cee26c4acd7941dbf597bae7c8
ahmadzai <wazir.ahmadzai@gmail.com>
2025-05-25 12:30:09 +0200

    Project changed to Pipenv for better dependency management

--- thecharmi/settings.py
+++ thecharmi/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-w$x9pezwu+1x79l@uq)m6*+=)!_%)113)ij@txd*s+i+jld8y!'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 # For Debug Toolbar
 INTERNAL_IPS = [
@@ -93,7 +94,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'thecharmi_db',
         'USER': 'postgres',  # Replace with your PostgreSQL username
-        'PASSWORD': 'super',  # Replace with your PostgreSQL password
+        'PASSWORD': config('DB_PASSWORD'),
         'HOST': 'localhost',
         'PORT': '5432',
     }
