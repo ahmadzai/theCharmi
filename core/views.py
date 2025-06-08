bd79093e947e83c91d3f40d48fc1b3af57a8ef8b
ahmadzai <wazir.ahmadzai@gmail.com>
2025-06-08 17:46:22 +0200

    redesigned static home page with slider was added

--- core/views.py
+++ core/views.py
@@ -3,20 +3,24 @@ from .models import HomeContent, Slider
 
 
 def home(request):
-    home_content = HomeContent.objects.all() # load all home contents,
-    # we will add a flag to identify some contents as inactive in the model
-    sliders = Slider.objects.all()
+    # home_content = HomeContent.objects.all() # load all home contents,
+    # # we will add a flag to identify some contents as inactive in the model
+    # sliders = Slider.objects.all()
+    #
+    # if not home_content.exists():
+    #     context = {
+    #         'home_content': [],
+    #         'sliders': sliders,
+    #         'message': 'No home content available at the moment.'
+    #     }
+    # else:
+    #     context = {
+    #         'home_content': home_content,
+    #         'sliders': sliders
+    #     }
 
-    if not home_content.exists():
-        context = {
-            'home_content': [],
-            'sliders': sliders,
-            'message': 'No home content available at the moment.'
-        }
-    else:
-        context = {
-            'home_content': home_content,
-            'sliders': sliders
-        }
-
-    return render(request, 'core/home.html', context)
+    context = {
+        'home_content': '',
+        'sliders': ''
+    }
+    return render(request, 'core/home_static.html', context)
